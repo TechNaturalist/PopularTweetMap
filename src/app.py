e7d5c8033d869ea2963beaa8b0146888fde2fcad
Jay Peterson <jay.peterson@aggiemail.usu.edu>
2021-08-04 17:41:01 -0600

    removed default routes, had to change requirements.txt a little

--- src/app.py
+++ src/app.py
@@ -5,25 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
-
-@app.route("/hello/")
-
-@app.route("/hello/<name>")
-def hello_there(name = None):
     return render_template(
-        "hello_there.html",
-        name=name,
-        date=datetime.now()
+        "index.html"
     )
 
-@app.route("/index/")
-def index(name = None):
-    return render_template(
-        "index.html",
-        name=name,
-        date=datetime.now()
-    )
 
 if __name__ == '__main__':
     app.run(debug = True) 
