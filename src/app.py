357df7425790f8e447d0d5fdffb0e5d308eb05bd
swede <swede.struve@aggiemail.usu.edu>
2021-08-09 21:47:50 -0600

    Integrate GET request

--- src/app.py
+++ src/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, abort, Response
 from flask.helpers import url_for
+from flask import request
+from flask import jsonify
 
 from sentiment_analysis.gather import Gather
 from sentiment_analysis.sentiment_analyzer import SentimentAnalyzer
+from sentiment_analysis.TweetSentiment import TweetSentiment
 
 app = Flask(__name__)
 
@@ -13,20 +16,29 @@ def home():
     )
 
 @app.route("/trends", methods=["GET"])
-def get_trends(latitude, longitude, trend_count = 1, limit = 50):
+def get_trends(trend_count = 1, limit = 50):
+  latitude = request.args.get('latitude')
+  longitude = request.args.get('longitude')
   if latitude == None:
     abort(Response("Latitude is required, but wasn't provided."))
   if longitude == None:
     abort(Response("Longitude is required, but wasn't provided."))
 
-  gatherer = Gather()
-  sentiment_analyzer = SentimentAnalyzer()
-  tweets = gatherer.newTweets(latitude, longitude, trend_count, limit)
-  tweets = sentiment_analyzer.add_sentiment_scores(tweets)
+  #gatherer = Gather()
+  #sentiment_analyzer = SentimentAnalyzer()
+  #tweets = gatherer.newTweets(latitude, longitude, trend_count, limit)
+  #tweets = sentiment_analyzer.add_sentiment_scores(tweets)
 
   ## do some logic to get a list of TweetSentiment out of tweets
-  sentiments = []
-  return sentiments
+  test_sentiment = TweetSentiment()
+  test_sentiment.trending_keyword = 'olympics'
+  test_sentiment.major_topics = [ 'gold', 'silver', 'bronze' ]
+  test_sentiment.num_negative_tweets = 100
+  test_sentiment.num_negative_tweets = 10
+  sentiments = [
+    test_sentiment.get_data()
+  ]
+  return jsonify(sentiments)
 
 
 if __name__ == '__main__':
--- src/sentiment_analysis/TweetSentiment.py
+++ src/sentiment_analysis/TweetSentiment.py
@@ -4,3 +4,11 @@ class TweetSentiment:
     self.num_negative_tweets = 0
     self.trending_keyword = ''
     self.major_topics = []
+
+  def get_data(self):
+    return {
+      'num_positive_tweets': self.num_positive_tweets,
+      'num_negative_tweets': self.num_negative_tweets,
+      'trending_keyword': self.trending_keyword,
+      'major_topics': self.major_topics
+    }
