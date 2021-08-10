654893a145a16f161da9baf6d054128d5a4e4aed
Chandler Williams <24323985+chamo92@users.noreply.github.com>
2021-08-09 18:57:25 -0600

    Add start of /trends endpoint.

--- src/app.py
+++ src/app.py
@@ -1,5 +1,8 @@
-from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, abort, Response
+from flask.helpers import url_for
+
+from sentiment_analysis.gather import Gather
+from sentiment_analysis.sentiment_analyzer import SentimentAnalyzer
 
 app = Flask(__name__)
 
@@ -9,6 +12,22 @@ def home():
         "index.html"
     )
 
+@app.route("/trends", methods=["GET"])
+def get_trends(latitude, longitude, trend_count = 1, limit = 50):
+  if latitude == None:
+    abort(Response("Latitude is required, but wasn't provided."))
+  if longitude == None:
+    abort(Response("Longitude is required, but wasn't provided."))
+
+  gatherer = Gather()
+  sentiment_analyzer = SentimentAnalyzer()
+  tweets = gatherer.newTweets(latitude, longitude, trend_count, limit)
+  tweets = sentiment_analyzer.add_sentiment_scores(tweets)
+
+  return ""
+
 
 if __name__ == '__main__':
-    app.run(debug = True) 
+    # app.run(debug = True)
+    with app.test_request_context():
+      print(url_for('get_trends', latitude=20.20202, longitude=15.0203))
